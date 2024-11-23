53cb9c65689f4ccac66beeb0621998d82345ab9d
NicanorHs <nicanorhermoso137@gmail.com>
2024-11-23 18:02:57 -0400

    Se hizo un cambio en la plantilla index , cambios al recibir la temperatura de un entero a un flotante
    cambios

--- Grafica/img.py
+++ Grafica/img.py
@@ -1,15 +1,13 @@
 from flask import Flask, render_template, request, jsonify
 import json
 
-
 app = Flask(__name__)
-## Se comenzara hacer el menu para la interfaz grafica##
-
+#Se hizo un cambio en la plantilla index
 def cargar_base_de_datos():
-    ##leer la base de datos
+    # Cargar la base de datos inicial
     try:
-       with open('base_de_datos.json', 'r') as f:
-           return json.load(f)
+        with open('base_de_datos.json', 'r') as f:
+            return json.load(f)
     except FileNotFoundError:
         return {}
 
@@ -17,12 +15,10 @@ data = cargar_base_de_datos()
 
 @app.route('/')
 def index():
-    ## Principal 
     return render_template('index.html', data=data)
 
 @app.route('/agregar_imagen', methods=['POST'])
 def agregar_imagen():
-    ##Secundario
     maquina = request.form.get('maquina')
     fecha = request.form.get('fecha')
     ruta_imagen = request.form.get('ruta_imagen')
@@ -32,33 +28,29 @@ def agregar_imagen():
     if not maquina or not fecha or not ruta_imagen or not temperatura_maquina or not temperatura_ambiente:
         return jsonify({'message': 'Todos los campos son requeridos'}), 400
 
-    
     maquina_data = data.get(maquina, {})
     fechas = maquina_data.setdefault('fechas', {})
     fechas[fecha] = {
         'imagen': ruta_imagen,
-        'temperatura_maquina': int(temperatura_maquina),
-        'temperatura_ambiente': int(temperatura_ambiente)
+        'temperatura_maquina': float(temperatura_maquina),
+        'temperatura_ambiente': float(temperatura_ambiente)
     }
 
     data[maquina] = maquina_data
 
-    
     with open('base_de_datos.json', 'w') as f:
         json.dump(data, f, indent=4)
 
     return jsonify({'message': 'Imagen agregada exitosamente'})
 
-@app.route('/ver_imagen', methods=['POST'])
-def ver_imagen(): 
-    ## Prueba, sin imagenes no funcionara 
-    maquina = request.form.get('maquina')
-    fecha = request.form.get('fecha')
+@app.route('/ver_imagen', methods=['GET'])
+def ver_imagen():
+    maquina = request.args.get('maquina')
+    fecha = request.args.get('fecha')
 
     if not maquina or not fecha:
         return jsonify({'message': 'Máquina y fecha son requeridas'}), 400
 
-  
     maquina_data = data.get(maquina, {})
     imagen_data = maquina_data.get('fechas', {}).get(fecha, {})
 
@@ -67,7 +59,6 @@ def ver_imagen():
 
     ruta_imagen = imagen_data.get('imagen')
 
-    
     return render_template('imagen.html', ruta_imagen=ruta_imagen, imagen_data=imagen_data, maquina=maquina, fecha=fecha)
 
 if __name__ == '__main__':
