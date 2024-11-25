fdbcc3f5cbc4e20f09298e3f7157351611a6f973
Alexander <alexander30557049@gmail.com>
2024-11-25 19:26:39 -0400

    funcion1(act)

--- Procesamiento de Imagenes/Funcion1.py
+++ Procesamiento de Imagenes/Funcion1.py
@@ -14,22 +14,27 @@ def Datos1(name):
 
  mask = cv2.inRange(hsv, RojoL, RojoH)  
  mask1 = cv2.inRange(hsv, AmarilloL, AmarilloH)  
- mask2 = cv2.inRange(hsv, CianL, CianH)  
+ mask2 = cv2.inRange(hsv, CianL, CianH)
+ mask3 = cv2.inRange(hsv, NaranjaL, NaranjaH)
  T = mask.shape
  lista = []
  lista1 =[]
  lista2 =[]
+ lista3 =[]
 
  for i in mask:  
     lista+=list(i)
  for i in mask1:  
     lista1+=list(i)
  for i in mask2:  
-    lista2+=list(i) 
+    lista2+=list(i)
+ for i in mask3:  
+    lista3+=list(i)
 
  a1=0
  b1=0
  c1=0
+ d1=0
  for i in lista:  
     if int(i)==0:  
         a1+=1
@@ -41,8 +46,15 @@ def Datos1(name):
  for i in lista2:  
     if int(i)==0:  
         c1+=1
+ for i in lista3:  
+    if int(i)==0:  
+        d1+=1
  a = T[0]*T[1]-a1
  b = T[0]*T[1]-b1
  c = T[0]*T[1]-c1
- return (a/(a+b+c))*100,(b/(a+b+c))*100,(c/(a+b+c))*100,((a+b+c)/(T[0]*T[1]))*100
-# return 40 - 50,20 - 30,0 - 10, Porcentaje del analisis
+ d = T[0]*T[1]-d1
+ if a+b+d+c != 0:
+  return (a/(a+b+c+d))*100,(d/(a+b+c+d))*100,(b/(a+b+c+d))*100,(c/(a+b+c+d))*100,((a+b+c+d)/(T[0]*T[1]))*100
+ else:
+     return 0,0,0,0,0
+# return 40 - 50,30 - 40,20 - 30,0 - 10, Porcentaje del analisis
