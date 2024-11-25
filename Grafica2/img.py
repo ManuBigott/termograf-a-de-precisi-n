ea85b0d15e29578b3697b5bb3b693191ccb05b87
NicanorHs <nicanorhermoso137@gmail.com>
2024-11-25 16:44:20 -0400

    Se enlazo la funcion escalador a la interfaz grafica
    enlace de la interfaz

--- Grafica2/img.py
+++ Grafica2/img.py
@@ -4,8 +4,87 @@ import matplotlib.pyplot as plt
 import base64
 import io
 from datetime import datetime
-
+import cv2
+import numpy as np
 app = Flask(__name__)
+
+@app.route('/seleccionar_imagen', methods=['GET'])
+def seleccionar_imagen():
+    """Renderiza la página para seleccionar la imagen a procesar"""
+    return render_template('seleccionar_imagen.html', data=data)
+
+def escalador(ruta):
+    Azul_oscuroL, Azul_oscuroH = np.array([120, 50, 50], np.uint8), np.array([130, 255, 255], np.uint8)
+    AzulL, AzulH = np.array([180, 50, 50], np.uint8), np.array([190, 255, 255], np.uint8)
+    CianL, CianH = np.array([75, 50, 50], np.uint8), np.array([85, 255, 255], np.uint8)
+    VerdeL, VerdeH = np.array([60, 50, 50], np.uint8), np.array([70, 255, 255], np.uint8)
+    AmarilloL, AmarilloH = np.array([30, 50, 50], np.uint8), np.array([40, 255, 255], np.uint8)
+    NaranjaL, NaranjaH = np.array([15, 50, 50], np.uint8), np.array([25, 255, 255], np.uint8)
+    RojoL, RojoH = np.array([0, 50, 50], np.uint8), np.array([10, 255, 255], np.uint8)
+    Rojo_intensoL, Rojo_intensoH = np.array([120, 50, 50], np.uint8), np.array([130, 255, 255], np.uint8)
+
+    img = cv2.imread(ruta, -1)
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    mask = cv2.inRange(hsv, RojoL, RojoH)
+    mask1 = cv2.inRange(hsv, AmarilloL, AmarilloH)
+    mask2 = cv2.inRange(hsv, CianL, CianH)
+    T = mask.shape
+    lista = []
+    lista1 = []
+    lista2 = []
+
+    for i in mask:
+        lista += list(i)
+    for i in mask1:
+        lista1 += list(i)
+    for i in mask2:
+        lista2 += list(i)
+
+    a1 = 0
+    b1 = 0
+    c1 = 0
+    for i in lista:
+        if int(i) == 0:
+            a1 += 1
+
+    for i in lista1:
+        if int(i) == 0:
+            b1 += 1
+
+    for i in lista2:
+        if int(i) == 0:
+            c1 += 1
+
+    a = T[0] * T[1] - a1
+    b = T[0] * T[1] - b1
+    c = T[0] * T[1] - c1
+
+    return {
+        "40_50": (a / (a + b + c)) * 100,
+        "20_30": (b / (a + b + c)) * 100,
+        "0_10": (c / (a + b + c)) * 100,
+        "analizado": ((a + b + c) / (T[0] * T[1])) * 100
+    }
+# Nueva ruta para procesar una imagen y obtener los resultados del análisis
+@app.route('/procesar_imagen', methods=['GET'])
+def procesar_imagen():
+    maquina = request.args.get('maquina')
+    fecha = request.args.get('fecha')
+
+    if not all([maquina, fecha]):
+        return jsonify({'message': 'Máquina y fecha son requeridas'}), 400
+
+    maquina_data = data.get(maquina, {})
+    imagen_data = maquina_data.get('fechas', {}).get(fecha, {})
+
+    if not imagen_data:
+        return jsonify({'message': 'Imagen no encontrada'}), 404
+
+    ruta_imagen = imagen_data.get('imagen')
+    resultados = escalador(ruta_imagen)  # Llama a la función escalador para procesar la imagen
+
+    return render_template('procesar_imagen.html', resultados=resultados)
 
 def cargar_base_de_datos():
     """Cargar la base de datos inicial"""
