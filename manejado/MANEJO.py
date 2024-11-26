04216409de64db12b276153c7ec4e151c170feba
GiovanniOmg <omogrossogiova12@gmail.com>
2024-11-26 19:53:50 -0400

    COHERENCIA CON MANEJO Y LOS DEMAS ARCHIVOS

--- manejado/MANEJO.py
+++ manejado/MANEJO.py
@@ -5,20 +5,20 @@ import datetime
 def lectura(archivo):
     with open(archivo) as archivo_json:
         datos=json.load(archivo_json)
-        print(datos['Maquina_1']['fechas']['10-11-2024']['imagen'])
         return datos
 def Carpeta_original(archivos):
-    print(archivos)
-    pos=archivos.index('Thermal image of equipment (Induction Motor) + 40 Ground Truths added')
-    carpetas_imagenes=archivos[pos]
-    ruta=os.path.abspath(carpetas_imagenes)
-    return os.listdir(carpetas_imagenes),ruta
+    print(archivos,end='\n\n')
+    for elm in archivos:
+        if not '.' in elm:   
+            carpetas_imagenes=elm
+            ruta_imagenes=directorio+'\\'+elm
+            return os.listdir(ruta_imagenes),ruta_imagenes
 def rutas_carpetas_imagenes(carpetas):
     combo=[]
     for carpeta in carpetas:
         if '.' in carpeta:
             continue
-        ruta_elementos_carpetas=ruta_madre+'/'+carpeta
+        ruta_elementos_carpetas=ruta_madre+'\\'+carpeta
         imagenes_carpeta=nombre_elementos(ruta_elementos_carpetas)
         combo.append(imagenes_carpeta)
     return combo
@@ -26,7 +26,7 @@ def nombre_elementos(rutas):
     rutas_imagen=[]
     imagenes=os.listdir(rutas)
     for imagen in imagenes:
-        rutas_imagenes=rutas+'/'+imagen
+        rutas_imagenes=rutas+'\\'+imagen
         rutas_imagen.append(rutas_imagenes)
     return rutas_imagen
 def muestreo(imagenes):
@@ -54,10 +54,14 @@ def cargado(archivo):
         json.dump(base,dicc,indent=4)
         print("Exportado")     
 base=lectura('manejado/base_de_datos.json')
-carpetas,ruta_madre=Carpeta_original(os.listdir())
+directorio=os.getcwd()+'\\'+'manejado'
+print(directorio, end='\n\n')
+carpetas,ruta_madre=Carpeta_original(os.listdir(directorio))
+print(carpetas)
 print(carpetas)
 imagenes=rutas_carpetas_imagenes(carpetas)
 
 #muestreo(imagenes)
 archivito=agregado(base,imagenes)
 cargado(archivito)
+
